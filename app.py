497a2fea001b3f1e0505d0a57ecd3c3e63617007
omprakash1717 <omprakashbadakar24@gmail.com>
2026-06-11 03:04:01 +0530

    Fix SQLite version issue for ChromaDB on Render

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import sys
+try:
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 import os
 from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
